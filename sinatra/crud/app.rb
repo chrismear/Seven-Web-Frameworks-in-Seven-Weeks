6c74f9139ac786cc062b480443e2620c10d9b5cb
Chris Tucker Mear <chris@feedmechocolate.com>
2023-05-09 10:41:36 +0100

    Add index HTML view using ERb

--- sinatra/crud/app.rb
+++ sinatra/crud/app.rb
@@ -15,12 +15,23 @@ end
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/bookmarks.db")
 DataMapper.finalize.auto_upgrade!
 
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
 def get_all_bookmarks
   Bookmark.all(:order => :title)
 end
 
 def get_all_bookmarks_ordered_by_created_at
   Bookmark.all(:order => :created_at)
+end
+
+get "/" do
+  @bookmarks = get_all_bookmarks
+  erb :bookmark_list
 end
 
 get "/bookmarks" do
